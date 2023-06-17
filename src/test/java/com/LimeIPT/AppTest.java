2d98d0183628502b13f5740a35c58576527af5b5
skull1234567 <skulln95@gmail.com>
2023-06-17 12:17:10 +0530

    updateeedd

--- src/test/java/com/LimeIPT/AppTest.java
+++ src/test/java/com/LimeIPT/AppTest.java
@@ -5,9 +5,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
-    
-  @Test
-  public void shouldAnswerWithTrue() {
-    assertTrue(true);
+	public void shouldAnswerWithTrue() {
+	    System.out.println("naveen");
+	  }
+	  public void main() {
+		  System.out.println("mee");
+	  }
+	  public static void main(String[] args) {
+		AppTest app = new AppTest();
+		app.shouldAnswerWithTrue();
+		app.main();
   }
 }
